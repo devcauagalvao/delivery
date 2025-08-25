dc5c14f8695b90bae218c72f662994a93034c13a
EduardoHenriqueDev <eduhenrimacedo@gmail.com>
2025-08-25 17:43:36 -0300

    GLV

--- components/header.tsx
+++ components/header.tsx
@@ -209,7 +209,7 @@ export default function Header({
                 </span>
 
                 {profile?.role === 'admin' && (
-                  <Link href="/admin">
+                  <Link href="https://delivery-admin-chi.vercel.app/">
                     <button
                       className={`${buttonStyles} bg-[#222222]/70 text-[#cc9b3b] border border-[#cc9b3b]`}
                     >
